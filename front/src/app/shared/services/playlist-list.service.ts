11bc506d6807f9c053f31b61bf9145c0e23fdf77
Anthony <anthonyoutub@gmail.com>
2023-02-14 13:08:21 +0100

    Ajout d'un service qui retourne les playlists en JSON pour les tests

--- front/src/app/shared/services/playlist-list.service.ts
+++ front/src/app/shared/services/playlist-list.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { IPlaylist } from '../models/playlist';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,64 @@ export class PlaylistListService {
 
   constructor() { }
 
-  public getPlaylist() : any {
-
+/**
+ * Fonction temporaire qui simule la reponse en JSON du back (pour faire des tests)
+ * @returns IPlaylist[] : la liste des hotels 
+ */
+  public getPlaylist(): IPlaylist[] {
+    return [
+      {
+        id: 1,
+        nom: "Daily Mix",
+        nomCreateur: "Antho",
+        nombreClics: 43,
+        contributeurs: "Lujipeka",
+        style: "French Rap",
+        morceauMusiqueListe: [{
+          id: 1,
+          titre: "Pas à ma place",
+          nomArtiste: "Lujipeka"
+        }, {
+          id: 2,
+          titre: "Metaverse",
+          nomArtiste: "Lujipeka"
+        }]
+      }, {
+        id: 2,
+        nom: "Noel 2023",
+        nomCreateur: "Lulu",
+        nombreClics: 98,
+        contributeurs: "Le père noel",
+        style: "Pop",
+        morceauMusiqueListe: [{
+          id: 3,
+          titre: "All I Want For Christmas Is You",
+          nomArtiste: "Mariah Carey"
+        }, {
+          id: 4,
+          titre: "Have Yourself a Merry Little Christmas",
+          nomArtiste: "Michael Bublé"
+        }]
+      }, {
+        id: 3,
+        nom: "Summer 2023",
+        nomCreateur: "Max",
+        nombreClics: 65,
+        contributeurs: "DavLaMenace",
+        style: "Pop",
+        morceauMusiqueListe: [{
+          id: 4,
+          titre: "Summer",
+          nomArtiste: "Calvin Harris"
+        }, {
+          id: 5,
+          titre: "Still Corners",
+          nomArtiste: "The Trip"
+        }]
+      }
+    ]
   }
+
+
+
 }
