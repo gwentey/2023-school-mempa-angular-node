11d9ec7fb6fb153c56c9b0cfaadf0fa3ee025782
Lucas Garlaschi <lucas.garlaschi@yahoo.com>
2023-02-27 21:50:10 +0100

    créatuib des classes objets

---
+++ morceau.js
@@ -0,0 +1,10 @@
+module.exports = class Morceau {
+
+    //Constructeur de la classe
+    constructor(titre, nomArtiste) {
+        this.titre = titre;
+        this.nomArtiste = nomArtiste;
+    }
+
+
+}
